eef853fdff7699db5b4aa5c0f0a293657fe63d0b
tanode <gaetano25@hotmail.it>
2018-12-26 18:48:16 +0100

    aggiunto controllo LCD

--- sensoreA1/sensoreA1.ino
+++ sensoreA1/sensoreA1.ino
@@ -24,7 +24,8 @@
 //---------------------------------------------------------------------
 //                           LIBRARIES                                
 //---------------------------------------------------------------------
-
+// include the library code:
+#include <LiquidCrystal.h>
 #include <NewPing.h>
 
 //---------------------------------------------------------------------
@@ -56,8 +57,11 @@ unsigned int timeCounter;
 unsigned int lightTimer = 0;
 unsigned int distanceTimer = 0;
 
+LiquidCrystal LCD(8, 7, 12, 11, 10, 9);
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);   // Creates a sonar object for controlling the ultrasonic
-void fastPrint(String string, int digit);   //prototipo metodo compatto stampa stringa;
+
+void lcdprint(int );
+void fastPrint(String , int );   //prototipo metodo compatto stampa stringa;
 
 //end
 //------------------------------------------------------------------
@@ -70,6 +74,20 @@ void fastPrint(String string, int digit);   //prototipo metodo compatto stampa s
 void setup() {
 
   Serial.begin(9600);   //START SERIAL MONITOR.
+  LCD.begin(16, 2);    // set up the number of columns and rows on the LCD object
+
+  
+
+  //AREA TEST LCD
+ 
+  LCD.print("test riga 1"); // Print a message to the LCD.
+
+  // set the cursor to column 0, line 1
+  // line 1 is the second row, since counting begins with 0
+  LCD.setCursor(0, 1);
+  LCD.print("test riga 2");  // print to the second line
+
+  delay(1500);
 
   
   // SETUP ULTRASONIC SENSOR
@@ -81,6 +99,13 @@ void setup() {
   pinMode(PHOTORESISTOR_PIN, INPUT);
   pinMode(PHOTORESISTOR_LED, OUTPUT);
 */
+
+// clean up the screen before printing a new reply
+  LCD.clear();
+  LCD.print("*AVVIO SONAR*");
+  delay(2000);
+  LCD.setCursor(0,0);
+  LCD.print("Distance(cm):");
  
 
 //end setup  
@@ -138,6 +163,7 @@ void loop() {
   //------------------------------------------------------------------------------
 
   Serial.println(distanceValue);  //for matlab readings
+  lcdprint(distanceValue);
   //------------------------------------------------------------------------------
 
   while (millis()-timeCounter < CLOCK){
@@ -166,3 +192,11 @@ void fastPrint(String string, int digit){
   Serial.println(digit);
   Serial.println();
 }
+
+//Function for printing distance on display
+void lcdprint(int distanceValue){
+  LCD.setCursor(13, 1);
+  LCD.print(distanceValue); // Convert ping time to distance in cm and print result (0 = outside set distance range)
+  LCD.print("     ");
+}
+
